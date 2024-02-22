a0a2d8a03aa1ddbc54233ebee55982b48677607e
leannep <leanne@lsst.org>
2024-02-22 11:59:12 -0300

    Convert document to latex

--- conf.py
+++
@@ -1,9 +0,0 @@
-"""Sphinx configuration.
-
-To learn more about the Sphinx configuration for technotes, and how to
-customize it, see:
-
-https://documenteer.lsst.io/technotes/configuration.html
-"""
-
-from documenteer.conf.technote import *  # noqa: F401, F403
